4f9cb8a41feb13236147b157a9128fdec638ede0
Tony Foerster <anthonymfoerster@gmail.com>
2022-12-04 11:51:08 -0500

    Fix installation

---
+++ ai/main.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+import openai
+
+
+def main():
+    firstArg = sys.argv[1]
+    if firstArg == "models":
+        models()
+    else:
+        print(complete(firstArg))
+
+
+def complete(prompt):
+    openai.api_key = token()
+    response = openai.Completion.create(model="text-davinci-003", prompt=prompt, temperature=0, max_tokens=1000)
+
+    return response.choices[0].text
+
+
+def models():
+    openai.api_key = token()
+    response = openai.Model.list(token())
+
+    print(response)
+
+
+def token():
+    return os.getenv("OPENAI_API_KEY")
+
+
+if __name__ == "__main__":
+    # models()
+    main()
